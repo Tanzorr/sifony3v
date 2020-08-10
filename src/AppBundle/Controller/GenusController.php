5d068759b57947daa5ad156b1d5acf752002e2ac
alex84 <alexx1984@ukr.net>
2020-08-10 18:15:29 +0300

    add react start

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -6,6 +6,7 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class GenusController extends Controller
 {
@@ -14,15 +15,38 @@ class GenusController extends Controller
      */
     public function showAction($genusName)
     {
-        $notes = [
-            'Octopus asked me a riddle, outsmarted me',
-            'I counted 8 legs.. as they wrapped around me',
-            'liked!'
-        ];
+
         return $this->render('genus/show.html.twig',[
             'name'=>$genusName,
-            'notes'=>$notes
+
             ]);
+    }
+
+    /**
+     * @Route("/genus/{genusName}/notes", name="genus_show_notes")
+     * @Method("GET")
+     */
+
+    public function getNotesAction()
+    {
+        $notes = [
+          ['id'=>1, 'username'=>'AquaPelham',
+              'avatrUri'=>'https://cdn3.vectorstock.com/i/1000x1000/30/97/flat-business-man-user-profile-avatar-icon-vector-4333097.jpg',
+              'note'=>'Inked!', 'date'=>'Aug. 20, 2015'],
+            ['id'=>1, 'username'=>'AquaPelham2',
+                'avatrUri'=>'https://cdn3.vectorstock.com/i/1000x1000/30/97/flat-business-man-user-profile-avatar-icon-vector-4333097.jpg',
+                'note'=>'Inked!', 'date'=>'Aug. 20, 2015'],
+            ['id'=>1, 'username'=>'AquaPelham3',
+                'avatrUri'=>'https://cdn3.vectorstock.com/i/1000x1000/30/97/flat-business-man-user-profile-avatar-icon-vector-4333097.jpg',
+                'note'=>'Inked!', 'date'=>'Aug. 20, 2015'],
+        ];
+
+        $data = [
+            'notes'=>$notes,
+        ];
+
+        return new Response(json_encode($data));
+
     }
 
 }
